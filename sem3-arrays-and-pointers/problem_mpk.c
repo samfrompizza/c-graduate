1d2f2011bbe12fb1045b653e5db11bcdb62af3d9
gasem <al.mazharish.g@phystech.edu>
2025-08-03 23:10:34 +0300

    hw started, TODO optimisations

--- sem3-arrays-and-pointers/problem_mpk.c
+++ sem3-arrays-and-pointers/problem_mpk.c
@@ -17,14 +17,14 @@ Poly mult(Poly lhs, Poly rhs)
     ret.p[0] = lhs.p[0] * rhs.p[0];
     return ret;
   } else {
-    Poly lhs1 = {lhs.n / 2, lhs.p + ((lhs.n /2) * sizeof(unsigned long long))};
+    Poly lhs1 = {lhs.n / 2, lhs.p + (lhs.n /2)};
     Poly lhs0 = {lhs.n / 2, lhs.p};
     Poly lhs10 = {lhs.n / 2, NULL};
     lhs10.p = calloc(lhs10.n, sizeof(unsigned long long));
     for (unsigned long long i = 0; i < lhs10.n; ++i)
       lhs10.p[i] = lhs0.p[i] + lhs1.p[i];
 
-    Poly rhs1 = {rhs.n / 2, rhs.p + ((rhs.n /2) * sizeof(unsigned long long))};
+    Poly rhs1 = {rhs.n / 2, rhs.p + (rhs.n /2)};
     Poly rhs0 = {rhs.n / 2, rhs.p};
     Poly rhs10 = {rhs.n / 2, NULL};
     rhs10.p = calloc(rhs10.n, sizeof(unsigned long long));
@@ -35,14 +35,29 @@ Poly mult(Poly lhs, Poly rhs)
     Poly second = mult(lhs10, rhs10);
     Poly third = mult(lhs0, rhs0);
 
-    for (unsigned long long i = 0; i < first.n; ++i)
-      ret.p[ret.n - first.n + i] += first.p[i];
+    unsigned long long half = lhs.n / 2;
+    for (unsigned long long i = 0; i < ret.n; ++i) {
+        unsigned long long acc = 0;
+    
+        if (i < third.n) {
+            acc += third.p[i];
+        }
+      
+        if (i >= half && i < half + second.n) {
+            unsigned long long idx = i - half;
+            unsigned long long t2 = second.p[idx];
+            unsigned long long t1 = (idx < first.n  ? first.p[idx]  : 0);
+            unsigned long long t3 = (idx < third.n ? third.p[idx] : 0);
+            acc += t2 - t1 - t3;
+        }
+      
+        if (i >= lhs.n && i < lhs.n + first.n) {
+            acc += first.p[i - lhs.n];
+        }
+      
+        ret.p[i] = acc;
+    }
 
-    for (unsigned long long i = 0; i < second.n; ++i)
-      ret.p[ret.n / 2 - second.n + i] += (second.p[i] - first.p[i] - third.p[i]);
-
-    for (unsigned long long i = 0; i < third.n; ++i)
-      ret.p[i] += third.p[i];
 
     free(lhs10.p);
     free(rhs10.p);
@@ -50,44 +65,42 @@ Poly mult(Poly lhs, Poly rhs)
     free(second.p);
     free(third.p);
 
-    unsigned long long n = ret.n;
-
-    for (unsigned long long i = 0; i < n; ++i) {
-      if (ret.p[n - 1 - i] == 0) {
-        ret.n -= 1;
-      } else {
-      return ret;;
-      }
-    }
-
     return ret;
   }
 }
 
 void poly_print(Poly ret)
 {
-  for (unsigned long long i = 0; i < ret.n; ++i)
-  {
-    printf("%lld ", ret.p[i]);
+  unsigned long long last = 0;
+  for (unsigned long long i = ret.n - 1; i > 0; --i) {
+    if (ret.p[i] != 0) {
+      last = i;
+      break;
+    }
   }
+  for (unsigned long long i = 0; i < last; ++i)
+  {
+    printf("%llu ", ret.p[i]);
+  }
+  printf("%llu", ret.p[last]);
   printf("\n");
 }
 
 int main()
 {
   Poly lhs, rhs;
-  if (scanf("%lld %lld", &lhs.n, &rhs.n) != 2)
+  if (scanf("%llu %llu", &lhs.n, &rhs.n) != 2)
     abort();
   lhs.p = calloc(lhs.n, sizeof(unsigned long long));
   rhs.p = calloc(rhs.n, sizeof(unsigned long long));
   for (unsigned long long i = 0; i < lhs.n; ++i)
   {
-    if (scanf("%lld", &lhs.p[i]) != 1)
+    if (scanf("%llu", &lhs.p[i]) != 1)
       abort();
   }
   for (unsigned long long i = 0; i < rhs.n; ++i)
   {
-    if (scanf("%lld", &rhs.p[i]) != 1)
+    if (scanf("%llu", &rhs.p[i]) != 1)
       abort();
   }
 

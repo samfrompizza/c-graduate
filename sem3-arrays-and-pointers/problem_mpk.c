069a51fb30e621d217f9e82f66fd9ca0f6b2d873
gasem <al.mazharish.g@phystech.edu>
2025-08-04 12:56:37 +0300

    robot test passed

--- sem3-arrays-and-pointers/problem_mpk.c
+++ sem3-arrays-and-pointers/problem_mpk.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <assert.h>
 
+// Max power, when naive method is used
+#define BASE_THRESHOLD 64
+
 typedef struct
 {
   unsigned long long n;
@@ -13,67 +16,67 @@ Poly mult(Poly lhs, Poly rhs)
   Poly ret = {rhs.n + lhs.n - 1, NULL};
   ret.p = calloc(ret.n, sizeof(unsigned long long));
 
-  if (ret.n == 1) {
-    ret.p[0] = lhs.p[0] * rhs.p[0];
-    return ret;
-  } else {
-    Poly lhs1 = {lhs.n / 2, lhs.p + (lhs.n /2)};
-    Poly lhs0 = {lhs.n / 2, lhs.p};
-    Poly lhs10 = {lhs.n / 2, NULL};
-    lhs10.p = calloc(lhs10.n, sizeof(unsigned long long));
-    for (unsigned long long i = 0; i < lhs10.n; ++i)
-      lhs10.p[i] = lhs0.p[i] + lhs1.p[i];
-
-    Poly rhs1 = {rhs.n / 2, rhs.p + (rhs.n /2)};
-    Poly rhs0 = {rhs.n / 2, rhs.p};
-    Poly rhs10 = {rhs.n / 2, NULL};
-    rhs10.p = calloc(rhs10.n, sizeof(unsigned long long));
-    for (unsigned long long i = 0; i < rhs10.n; ++i)
-      rhs10.p[i] = rhs0.p[i] + rhs1.p[i];
-
-    Poly first = mult(lhs1, rhs1);
-    Poly second = mult(lhs10, rhs10);
-    Poly third = mult(lhs0, rhs0);
-
-    unsigned long long half = lhs.n / 2;
-    for (unsigned long long i = 0; i < ret.n; ++i) {
-        unsigned long long acc = 0;
-    
-        if (i < third.n) {
-            acc += third.p[i];
-        }
-      
-        if (i >= half && i < half + second.n) {
-            unsigned long long idx = i - half;
-            unsigned long long t2 = second.p[idx];
-            unsigned long long t1 = (idx < first.n  ? first.p[idx]  : 0);
-            unsigned long long t3 = (idx < third.n ? third.p[idx] : 0);
-            acc += t2 - t1 - t3;
-        }
-      
-        if (i >= lhs.n && i < lhs.n + first.n) {
-            acc += first.p[i - lhs.n];
-        }
-      
-        ret.p[i] = acc;
+  // Naive algorythm in BASE case
+  if (ret.n <= BASE_THRESHOLD)
+  {
+    for (unsigned long long i = 0; i < lhs.n; ++i)
+    {
+      for (unsigned long long j = 0; j < rhs.n; ++j)
+      {
+        ret.p[i + j] += lhs.p[i] * rhs.p[j];
+      }
     }
-
-
-    free(lhs10.p);
-    free(rhs10.p);
-    free(first.p);
-    free(second.p);
-    free(third.p);
-
     return ret;
   }
+
+  // Karacuba algorythm
+  unsigned long long half = lhs.n / 2;
+  assert(lhs.n == rhs.n);
+
+  Poly lhs1 = {half, lhs.p + (half)};
+  Poly lhs0 = {half, lhs.p};
+  Poly lhs10 = {half, calloc(lhs10.n, sizeof(unsigned long long))};
+  Poly rhs1 = {half, rhs.p + (half)};
+  Poly rhs0 = {half, rhs.p};
+  Poly rhs10 = {half, calloc(rhs10.n, sizeof(unsigned long long))};
+  // (lhs10.n == rhs10.n) because of (lhs.n == rhs.n)
+  for (unsigned long long i = 0; i < lhs10.n; ++i)
+  {
+    lhs10.p[i] = lhs0.p[i] + lhs1.p[i];
+    rhs10.p[i] = rhs0.p[i] + rhs1.p[i];
+  }
+
+  // Recursive calculating of A1B1, ((A0+A1)(B0+B1)), A0B0 
+  Poly first = mult(lhs1, rhs1);
+  Poly second = mult(lhs10, rhs10);
+  Poly third = mult(lhs0, rhs0);
+
+  // AB = A1B1*x^n + ((A0+A1)(B0+B1)-A1B1-A0B0)*x^half + A0B0
+  for (unsigned long long i = 0; i < third.n; i++)
+    ret.p[i] += third.p[i];
+
+  for (unsigned long long i = 0; i < second.n; i++)
+    ret.p[i + half] += second.p[i] - first.p[i] - third.p[i];
+
+  for (unsigned long long i = 0; i < first.n; i++)
+    ret.p[i + 2 * half] += first.p[i];
+
+  free(lhs10.p);
+  free(rhs10.p);
+  free(first.p);
+  free(second.p);
+  free(third.p);
+
+  return ret;
 }
 
 void poly_print(Poly ret)
 {
   unsigned long long last = 0;
-  for (unsigned long long i = ret.n - 1; i > 0; --i) {
-    if (ret.p[i] != 0) {
+  for (unsigned long long i = ret.n - 1; i > 0; --i)
+  {
+    if (ret.p[i] != 0)
+    {
       last = i;
       break;
     }

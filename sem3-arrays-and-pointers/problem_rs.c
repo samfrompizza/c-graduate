a2c57e05f4a232b59f2ab5bb3dc023a9754b8afc
gasem <al.mazharish.g@phystech.edu>
2025-08-07 00:38:53 +0300

    radix sort step

--- sem3-arrays-and-pointers/problem_rs.c
+++ sem3-arrays-and-pointers/problem_rs.c
@@ -4,7 +4,8 @@
 
 #define NOTATION 10
 
-int pow(int base, int m) {
+int power(int base, int m)
+{
   int pow = 1;
   for (int i = 0; i < m; ++i)
     pow *= base;
@@ -12,29 +13,58 @@ int pow(int base, int m) {
   return pow;
 }
 
-void radix_step(int n, int m, int *arr) {
-  int *buff = calloc(NOTATION, sizeof(int));
-  int *incl_scan = calloc(NOTATION, sizeof(int));
-  for (int i = 0; i < n; ++i)
-    buff[(arr[i] % pow(NOTATION, m)) / NOTATION] += 1; 
+void radix_step(int n, int m, int *arr)
+{
+  int *count = calloc(NOTATION, sizeof(int));
+  int *prefix = calloc(NOTATION, sizeof(int));
+  int *output = calloc(n, sizeof(int));
 
-  for (int i = 0; i < n; ++i) {}
+  for (int i = 0; i < n; i++) {
+    int d = (arr[i] / power(NOTATION, m)) % NOTATION;
+    count[d]++;
+  }
 
+  prefix[0] = count[0];
+  for (int i = 1; i < NOTATION; i++) {
+    prefix[i] = prefix[i-1] + count[i];
+  }
+
+  for (int i = n-1; i >= 0; i--) {
+    int d = (arr[i] / power(NOTATION, m)) % NOTATION;
+    output[prefix[d] - 1] = arr[i];
+    prefix[d]--;
+  }
+
+  for (int i = 0; i < n; i++) {
+    arr[i] = output[i];
+  }
+
+  free(count);
+  free(prefix);
+  free(output);
 }
 
-int main() {
+int main()
+{
   int n, m;
   if (scanf("%d", &n) != 1)
     abort();
   int *arr = calloc(n, sizeof(int));
-  for (int i = 0; i < n; ++i) {
+  for (int i = 0; i < n; ++i)
+  {
     if (scanf("%d", &arr[i]) != 1)
       abort();
   }
   if (scanf("%d", &m) != 1)
     abort();
 
-  
+  radix_step(n, m, arr);
+
+  for (int i = 0; i < n; ++i)
+    printf("%d ", arr[i]);
+  printf("\n");
+
+  free(arr);
 
   return 0;
 }
